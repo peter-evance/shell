0a688509fd9aec140c78a840a94f82d7ec8454ac
Peter Evance <59042622+peter-evance@users.noreply.github.com>
2023-09-16 20:41:37 +0300

    Update main.c - 2

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include "shell.h"
 
 /**
- * main - Entry point for the shell program.
- * @ac: Argument count.
- * @av: Argument vector.
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: 0 on success, 1 on error.
+ * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
 {
-    	shell_info info[] = { INFO_INIT };
+	info_t info[] = { INFO_INIT };
 	int fd = 2;
-	
+
 	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
@@ -25,12 +26,12 @@ int main(int ac, char **av)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				print_to_stderr(av[0]);
-		                print_to_stderr(": 0: Can't open ");
-		                print_to_stderr(av[1]);
-		                _eputchar('\n');
-		                _eputchar(BUF_FLUSH);
-		                exit(127);
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
